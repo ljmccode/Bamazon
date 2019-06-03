36beed8e951bf9168d60ec4ffafb385ad8be0aa2
Laura <lmcsay@gmail.com>
2019-06-03 17:02:04 -0500

    inquirer questions added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,6 +12,37 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    afterConnection()
   });
   
+  function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, row) {
+      if (err) throw err;
+      // console.log(row);
+      for (var i = 0; i<row.length; i++) {
+        console.log("***********************************");
+          console.log("Product ID: " + row[i].item_id);
+          console.log("Product Name: " + row[i].product_name);
+          console.log("Department: " + row[i].department_name);
+          console.log("Price: " + row[i].price);
+          console.log("Quantity: " + row[i].stock_quantity);        
+        }
+        transaction();
+        connection.end();
+    })
+}
+
+function transaction() {
+    inquirer.prompt([
+        {
+            name: "Product ID",
+            message: "What is the ID of the product you would like to buy?"
+        },
+        {
+            name: "Quantity",
+            message: "How many units would you like to buy?"
+        }
+    ]).then(function(answers) {
+
+    })
+}
